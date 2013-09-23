7c807e7b93bb5543c604e703510310eba5cf57c9
Leber Perez <leber.perez@blackbaud.com>
2013-09-23 15:42:20 -0500

    Adding friendlybot

---
+++ friendlyBot.js
@@ -0,0 +1,32 @@
+var Bot    = require('ttapi');
+var AUTH   = 'fPLcVsKRFxCuXDUJgdtQXnbT';
+var USERID = '523cbc4caaa5cd604208c221';
+var ROOMID = '5239ba6eaaa5cd1c65cd5115';
+
+var bot = new Bot(AUTH, USERID, ROOMID);
+
+bot.on('speak', function (data) {
+  // Get the data
+  var name = data.name;
+  var text = data.text;
+
+  if (name.match(/DJ Luminate/)){
+    bot.speak('Who are you, DJ Luminate?');
+  } else if (text.match(/^\/hello$/)) {
+    bot.speak('Hey! How are you @'+name+'?');
+  }
+});
+
+
+bot.on('newsong', function(data){
+  var songName = data.room.metadata.current_song.metadata.song;
+  var artist = data.room.metadata.current_song.metadata.artist;
+
+  if( Math.floor((Math.random()*2)) % 2 == 0){
+    bot.speak('OMG! ' + songName);
+  } else {
+    bot.speak('I just love ' + artist);
+  }
+
+  bot.vote('up');
+});
